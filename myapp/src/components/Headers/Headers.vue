b7088bacff784fabed682959678654095947b86b
caozhen-cx <870455681@qq.com>
2021-04-29 20:00:08 +0800

    add

--- myapp/src/components/Headers/Headers.vue
+++ myapp/src/components/Headers/Headers.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="head">
         <img src="@/assets/img/logo.png" alt="">
-
+        <div class="center">
+            <slot name="head_Center"></slot>
+        </div>
         <div class="right">
             <span>
-                <slot></slot>
+                <slot name="head_right"></slot>
             </span>
             <span><i class="iconfont icon-nav"></i>导航</span>
         </div>
