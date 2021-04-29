6d6eba3a72a6297e388c4b26b6cbde959cf3dabc
Jyf <8899612+jing-yunfei@user.noreply.gitee.com>
2021-04-29 19:27:36 +0800

    真实轮播

--- myapp/src/views/Index/Home.vue
+++ myapp/src/views/Index/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home">
-   
+    <swiper />
   </div>
   <!-- 主页面 -->
 </template>
 
 <script>
-
-
+import swiper from '@/components/Swiper'
 export default {
-  
-}
+  data() {
+    return {};
+  },
+  components: {
+    swiper,
+  },
+};
 </script>
