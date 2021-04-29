24f794bc14719a8a82d8641a8a41719c731577cf
Jyf <8899612+jing-yunfei@user.noreply.gitee.com>
2021-04-29 18:09:36 +0800

    轮播整理图片

---
+++ myapp/src/components/Swiper.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang='scss'>
+</style>
