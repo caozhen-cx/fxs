1608ca8b2bc3effe437dbedddab31a7b5086c3ad
caozhen-cx <870455681@qq.com>
2021-05-05 17:59:41 +0800

    修改错误

---
+++ myapp/src/views/broadcast/BroadcastDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 工地直播详情页 -->
+  <div class="">
+    <my-heads />
+
+   
+
+    <my-footer />
+    <my-ftel />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+</style>
--- myapp/src/views/broadcast/broadcast.js
+++ myapp/src/views/broadcast/broadcast.js
@@ -1,11 +1,12 @@
 export default [
   {
     path: "/broadcast",
-    component: () => import("@/views/broadcast/broadcast.vue"),
+    component: () => import("@/views/broadcast/Broadcast.vue"),
     // 工地直播
   },
   {
-    path: "/particulars",
-    component: () => import("@/views/broadcast/particulars.vue"),
+    path: "/broadcastDetails",
+    component: () => import("@/views/broadcast/BroadcastDetails.vue"),
+    // 工地直播详情
   },
 ];
