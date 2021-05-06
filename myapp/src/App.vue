a76c452a6eb7a0e0a84734843640f730c707c56f
caozhen-cx <870455681@qq.com>
2021-05-06 08:46:24 +0800

    切屏动画

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view />
+    <transition name="slide">
+      <router-view class="ani-pager" />
+    </transition>
     <!-- 回到顶部 -->
     <div class="fixed_top" @click="top">
       <a> <i class="icon-top iconfont"></i></a>
@@ -13,7 +15,7 @@ import "@/assets/iconfont/iconfont.css";
 export default {
   methods: {
     top() {
-      document.documentElement.scrollTop = 0;
+      document.querySelector(".ani-pager").scrollTop = 0;
     },
     // 返回顶部
   },
@@ -27,5 +29,23 @@ export default {
   list-style: none;
   text-decoration: none;
   font-size: 16px;
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s;
+}
+.slide-enter {
+  transform: translateX(100%);
+}
+.slide-leave-to {
+  transform: translateX(-100%);
+}
+.ani-pager {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
 }
 </style>
